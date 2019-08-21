fda40b3d5a3da0b7ea4c2583b287a1da73295748
Matheus Botelho <39282759+matheusbc97@users.noreply.github.com>
2019-08-21 11:21:49 -0300

    Atualizar resposta, progresso@Input()

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -10,11 +10,44 @@ import { FRASES } from './frases-mock'
 })
 export class PainelComponent implements OnInit {
 
-  public frases: Frase[] = FRASES
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a frase';
+  public resposta: string;
 
-  constructor() { console.log(this.frases) }
+  public rodada: number = 0;
+  public rodadaFrase: Frase ;
+
+  public progresso: number = 0;
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase) 
+  }
 
   ngOnInit() {
   }
 
+  atualizaResposta(resposta: Event): void{
+    this.resposta = (<HTMLInputElement> resposta.target).value
+    //console.log(this.resposta)
+  }
+
+  public verificaResposta(): void {
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert('A tradução está correta')
+
+      console.log('Verificar resposta: ', this.resposta);
+
+      this.rodada++;
+
+      this.progresso += (100/this.frases.length);
+      console.log(this.progresso)
+
+      this.rodadaFrase = this.frases[this.rodada];
+    }else{
+      alert('A tradução está errada')
+    }
+
+    
+  }
 }
